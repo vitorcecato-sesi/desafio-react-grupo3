2220e5a4fc04572af0469ad189cf20461b07a811
Laura Betti <bettilaura0@gamail.com>
2025-02-13 09:32:33 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      
+    <section> 
+      <Header /> 
+    </section> 
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import "./Header.css"
+import logoG3 from "../assets/logoG3.png"
 
 function Header() {
-
+    return (
+        <header className="Header"> 
+            <h1> DESAFIO 1 de PROPS - REACT </h1>
+            <img src={logoG3} />
+        </header>
+    )
 }
 
 export default Header
