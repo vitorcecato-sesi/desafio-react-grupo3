791184f2423f447d5519ba37b1aa0192316a2a36
Lucas C <lucascasagrandesesi@gmail.com>
2025-02-13 08:22:07 -0300

    Inserimento de 3 perfis no app.jsx e algumas alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import Perfil from "./components/Perfis"
-import Teste from "./assets/logoG3.png"
+
+
+
+import fotoPietro from "./assets/fotoUsuarioPietroM.png"
+import fotoPyetro from "./assets/fotoUsuarioPyetro.png"
+import fotoVitor from "./assets/fotoUsuarioVitorG.png"
 
 function App() {
 
   return (
     <>
-      <Perfil foto={Teste} nome="Nome Teste" hobbies="Esses são meus hobbies"/>
+
+
+
+      <Perfil foto={fotoPietro} nome="Pietro Melle Michelin" hobbies ="Os meus hobbies são: Praticar esportes"></Perfil>
+      <Perfil foto={fotoPyetro} nome="Pyetro Joaquim Taborda Nunes" hobbies ="Os meus hobbies são: Jogar jogos onlines, ouvir música e cozinhar"></Perfil>
+      <Perfil foto={fotoVitor} nome="Vitor Geraldo Cecato " hobbies ="Meus hobbies são: jogar jogos onlines, programar e nadar."></Perfil>
     </>
   )
 }
