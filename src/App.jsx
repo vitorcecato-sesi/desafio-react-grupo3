b3dbad3cb0a3cae16ff7b94b7c395ff0df6a1ac3
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-02-13 09:33:09 -0300

    Configurando o app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 /* 🎈 Aqui estamos importando o arquivo de estilo (CSS) */
 import "./App.css";
-/* 🎈 Aqui estamos importando o componente "Perfil", que vai trazar a function(props) para colocar as informações aqui */
-import Perfil from "./components/Perfis";
+
+// Componentes:
+  /* 🎈 Aqui estamos importando o componente "Perfil", que vai trazar a function(props) para colocar as informações aqui */
+  import Perfil from "./components/Perfis";
+//.
+
 /* 🎈 Aqui estamos importando as imagens que serão usadas nos perfis */
 import fotoPerfilLauraB from "./assets/fotoUsuarioLauraB.png";
 import fotoPerfilLucas from "./assets/fotoUsuarioLucas.png";
@@ -15,8 +19,9 @@ function App() {
 
   /* 🎈 Aqui estamos retornando o conteúdo do nosso aplicativo, o que vai exibir */
   return (
-    <div>
-      <>
+    <>
+    <main className="elementosCartas">
+      <section className="blocoCartas">
         {/* 🎈 Aqui estamos chamando o componente "Perfil" e passando as informações de cada pessoa */}
         <Perfil
           // Aqui estamos passando a foto como propriedade para o componente "Perfil"
@@ -27,18 +32,42 @@ function App() {
           hobbies="Ouvir música e ver filmes/séries."
         />
 
-        <Perfil foto={fotoPerfilLucas} nome="Lucas Casagrande da Silva" hobbies="Praticar vôlei, jogar jogos online e assistir séries." />
+        <Perfil 
+          foto={fotoPerfilLucas} 
+          nome="Lucas Casagrande da Silva" 
+          hobbies="Praticar vôlei, jogar jogos online e assistir séries." 
+        />
 
-        <Perfil foto={fotoPerfilMilena} nome="Milena Oliveira Souza" hobbies="Escutar música, ver filmes e séries, estudar matemática e cozinhar." />
+        <Perfil 
+          foto={fotoPerfilMilena} 
+          nome="Milena Oliveira Souza" 
+          hobbies="Escutar música, ver filmes e séries, estudar matemática e cozinhar." 
+        />
+      </section>
 
-        <Perfil foto={fotoPietro} nome="Pietro Melle Michelin" hobbies="Os meus hobbies são: Praticar esportes" />
+      <section className="blocoCartas">
+        <Perfil 
+          foto={fotoPietro} 
+          nome="Pietro Melle Michelin" 
+          hobbies="Os meus hobbies são: Praticar esportes" 
+        />
 
-        <Perfil foto={fotoPyetro} nome="Pyetro Joaquim Taborda Nunes" hobbies="Os meus hobbies são: Jogar jogos onlines, ouvir música e cozinhar" />
+        <Perfil 
+          foto={fotoPyetro} 
+          nome="Pyetro Joaquim Taborda Nunes" 
+          hobbies="Os meus hobbies são: Jogar jogos onlines, ouvir música e cozinhar" 
+        />
 
-        <Perfil foto={fotoVitor} nome="Vitor Geraldo Cecato" hobbies="Meus hobbies são: jogar jogos onlines, programar e nadar." />
-      </>
-    </div>
-  );
+        <Perfil 
+          foto={fotoVitor} 
+          nome="Vitor Geraldo Cecato" 
+          hobbies="Meus hobbies são: jogar jogos onlines, programar e nadar." 
+        />
+      </section>
+      </main>
+      
+    </>
+  )
 }
 
 /* 🎈 Aqui estamos exportando a função "App" para que ela possa ser usada em outros arquivos e o que vai iniciar o App */
