db907328a53b3b0a6565cd2d4f3cfa78c342bf37
Milena <mile.26souza08@gmail.com>
2025-02-13 07:35:02 -0300

    footer com props  criado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer(props) {
+
+    return (
+      <>
+      <footer className="footer">
+
+        <section className="informacoes-contatos">
+            <h3>{props.email}</h3>
+            <h3>{props.instagram}</h3>
+        </section>
+
+        <section className="informacao-resposavel" >
+            <p>{props.texto}</p>
+        </section>
+
+      </footer>
+        
+      </>
+    )
+  }
+  
+  export default Footer
