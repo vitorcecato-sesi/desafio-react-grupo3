535082b4c187d44d8773beedb5ef9b0b47cb3126
Extreme <npyetro26@gmail.com>
2025-02-13 08:56:22 -0300

    Aside completa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Valores from './components/aside'
 
 function App() {
 
   return (
     <>
+      
+      <Valores  valor1="Valores" valor2="Barigandandadnanda" valor3="babababab"/>
       
     </>
   )
