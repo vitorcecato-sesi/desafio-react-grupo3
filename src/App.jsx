b999d3619ce5904f73c0a570140524d0db693232
PietroMelle <mellepietro3@gmail.com>
2025-02-13 08:23:22 -0300

    3 perfis primeiros

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Perfil from "./components/Perfis"
-import Teste from "./assets/logoG3.png"
+import fotoPerfilLauraB from "./assets/fotoUsuarioLauraB.png"
+import fotoPerfilLucas from "./assets/fotoUsuarioLucas.png"
+import fotoPerfilMilena from "./assets/fotoUsuarioMilena.png"
 
 function App() {
 
   return (
     <>
-      <Perfil foto={Teste} nome="Nome Teste" hobbies="Esses são meus hobbies"/>
+      <Perfil foto={fotoPerfilLauraB} nome="Laura Betti Migliaccio" hobbies="Ouvir música e ver filmes/séries."/>
+
+      <Perfil foto={fotoPerfilLucas} nome="Lucas Casagrande da Silva" hobbies="Praticar vôlei, jogar jogos online e assistir séries."/>
+
+      <Perfil foto={fotoPerfilMilena} nome="Milena Oliveira Souza" hobbies="Escutar música, ver filmes e séries, estudar matemática e cozinhar."/>
     </>
   )
 }
