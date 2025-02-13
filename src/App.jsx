929bba4a8b4c99167b0515e104b56de40cf9830c
Milena <mile.26souza08@gmail.com>
2025-02-13 07:45:33 -0300

    footer importado no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <>
       
+      <Footer email="vitor.cecatosesi@gmail.com" instagram="@vitorgcecato" texto="O líder Vitor Geraldo Cecato é responsável pelo projeto referente ao conteúdo PROPS."/>
     </>
   )
 }
